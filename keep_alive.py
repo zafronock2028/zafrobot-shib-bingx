8ac534dcd548167bec67a8f89bfa98db997049bd
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-04-26 19:18:31 -0400

    Crear keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "ZafroBot está funcionando correctamente."
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
