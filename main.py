4eca6e0f4f80ddd72c3286cdd1834a14b75fc382
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-04-25 23:04:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+# Lee las claves desde las variables de entorno
+API_KEY = os.getenv("API_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 @app.route('/')
 def index():
-    return 'Zafrobot SHIB BingX está corriendo correctamente.'
+    return f'Zafrobot SHIB BingX activo. API: {API_KEY[:4]}****'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
