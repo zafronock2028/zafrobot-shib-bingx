dc7419425c63416b71769dc2006be265d2e0c734
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-04-26 19:34:33 -0400

    Actualizar keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "ZafroBot está funcionando correctamente."
+    return "✅ ZafroBot está funcionando correctamente."
 
 def run():
     app.run(host='0.0.0.0', port=10000)
