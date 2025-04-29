f3ce0bdf0133bc9933139635787513e6069e9198
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-04-29 02:15:47 -0400

    Actualizar start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-echo "======================================="
-echo "   Iniciando ZafroBot Dinámico Pro...   "
-echo "======================================="
-
-while true
-do
-  python main.py
-  echo "⚠️ El bot se detuvo. Reiniciando en 5 segundos..."
-  sleep 5
-done
+echo "Iniciando ZafroBot Dinámico Pro..."
+python3 main.py
